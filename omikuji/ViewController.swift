ef6dccb923930e0599bafbcba452b2c44827dd5e
遠山聡美 <S083@inv-238.inv.local>
2017-11-30 12:19:15 +0900

    Initial Commit

---
+++ omikuji/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  omikuji
+//
+//  Created by 遠山　聡美 on 2017/11/30.
+//  Copyright © 2017年 Simple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
